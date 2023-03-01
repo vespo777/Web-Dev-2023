5eb72403e93bab6bd85142b7c647c24067cb2e81
vespo777 <87870672+vespo777@users.noreply.github.com>
2023-03-01 21:17:40 +0600

    Add files via upload

---
+++ lab5/src/app/app/app.component.ts
@@ -0,0 +1,13 @@
+// import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../products';
+
+
+@Component({
+  selector: 'app-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+}
---
+++ lab5/src/app/category/category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+// import { products } from '../products';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent {
+
+}
